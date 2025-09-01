6356e5f45426bbb110a486ef23d8accbbc3ca50e
Yonatan Gorovetz <goro5357@gmail.com>
2025-09-01 16:58:52 +0300

    sequelize ex6 done

--- sequelize/pokemonExercises.ts
+++ sequelize/pokemonExercises.ts
@@ -67,4 +67,26 @@ async function findRoster(trainer:string){
     return pokemonNames;
 }
 
-findRoster("Loga");
+//Ex6
+async function findMostOwened() {
+    const results = await sequelize.query(
+        `SELECT p.name, COUNT(*) as count
+        FROM pokemon p
+        JOIN pokemon_trainer pt ON p.pokemon_id = pt.pokemon_id
+        GROUP BY p.pokemon_id, p.name
+        HAVING COUNT(*) = (
+            SELECT MAX(pokemon_count) 
+            FROM (
+                SELECT COUNT(*) as pokemon_count 
+                FROM pokemon_trainer 
+                GROUP BY pokemon_id
+            ) as counts
+        )`,
+        {type:QueryTypes.SELECT}
+    )
+    const pokemonNames = results.map((res:any) => res.name);
+    console.log(pokemonNames);
+    return pokemonNames;
+}
+
+findMostOwened();
